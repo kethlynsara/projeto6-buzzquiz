60b3a9e4b4269b2342eee52a67ccb490a3255655
Kethlyn <kethlynsaraa@gmail.com>
2022-02-21 22:01:32 -0300

    Primeira versão Tela 2: seleção de quizz para responder, exibição de rrespostas aleatoriamente, comportamento de respostas

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const BUZZQUIZZAPI = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 let titulo = document.querySelector(".tituloQuiz").value;
 let imagem = document.querySelector(".criarQuizInfoBasica .imagemQuiz").value;
 let inputLength;
+let quizzSelecionado = "";
+let idQuizz = "";
+
+
 
 /* -------------------------------------------- Layout Mobile (Tela 1) ----------------------------------------------- */
-// Requisição de todos os quizzes 
+
 function buscarQuizzes() {
     const quizzes = axios.get(`${BUZZQUIZZAPI}`)
     quizzes.then((message) => { exibirQuizzes(message) });
@@ -46,7 +50,6 @@ function validarUrl(url) {
 
 // Função que faz exibir todos os quizzes na tela
 function exibirQuizzes(resposta) {
-
     let main = document.querySelector(".main-tela1");
 
     main.innerHTML += `
@@ -70,13 +73,15 @@ function exibirQuizzes(resposta) {
     quizzes.forEach(quizz => {
         if (validarUrl(quizz.image) === true) {
             div.innerHTML += `
-                <div class="quizzesCriadosPorTodos-Imagens" onclick="aparecerTela2(this)">
+                <div class="quizzesCriadosPorTodos-Imagens" onclick="aparecerTela2(${quizz.id})">
                     <img src="${quizz.image}"/>
                     <p>${quizz.title}</p>
                 </div>
             `;
         }
     });
+
+    perguntasERespostasQuizz(quizzes);
 
 }
 buscarQuizzes();
@@ -101,19 +106,96 @@ function aparecerTelaCriarQuizzes() {
 
 /* -------------------------------------------- Layout Mobile (Tela 2) ----------------------------------------------- */
 
-function aparecerTela2(quizzSelecionado) {
-    console.log("executou");
-    const headerTela1 = document.querySelector(".header-tela1");
-    const mainTela1 = document.querySelector(".main-tela1");
-    const headerTela2 = document.querySelector(".header-tela2");
-    const mainTela2 = document.querySelector(".tela2");
-    //const telaFinalQuizz = document.querySelector("footer");
+function aparecerTela2(id) {
+    idQuizz = id;
+    let imagem = document.querySelector(".quizzesCriadosPorTodos-Imagens img");
+    let p = document.querySelector(".quizzesCriadosPorTodos-Imagens p");
+    let headerTela1 = document.querySelector(".header-tela1");
+    let mainTela1 = document.querySelector(".main-tela1");
+    let headerTela2 = document.querySelector(".header-tela2");
+    let mainTela2 = document.querySelector(".tela2");
     headerTela1.classList.add("escondido");
     mainTela1.classList.add("escondido");
     headerTela2.classList.remove("escondido");
     mainTela2.classList.remove("escondido");
-    // telaFinalQuizz.classList.add("escondido");
+
+    quizzSelecionado = (vetor.filter((obj) => obj.id === id))[0];
+    headerTela2.innerHTML = `<h1>BuzzQuizz</h1>`;
+    mainTela2.innerHTML += `
+            <!-- Título e imagem do quizz -->
+            <div class="quizz-header">
+                <img src="${quizzSelecionado.image}">
+                <p class="titulo-quizz">${quizzSelecionado.title}</p>
+            </div>
+            `;
+
+    for (let i = 0; i < quizzSelecionado.questions.length; i++) {
+        mainTela2.innerHTML += `
+        <div class="box-respostas box${i}">
+            <div class="pergunta"  style="background-color: ${quizzSelecionado['questions'][i]['color']}">
+                <p>${quizzSelecionado['questions'][i]['title']}</p>
+            </div>
+            <div class="respostas respostas${i}"></div>        
+        `;        
+        for (let j = 0; j < quizzSelecionado.questions[i].answers.length; j++) {
+            let respostas = document.querySelector(`.box${i} .respostas`);
+            quizzSelecionado.questions[i].answers.sort(comparador);
+            respostas.innerHTML +=`
+            <div class="resposta respostas${i}" onclick="vericarRespostas()">
+                <div class="opcao">
+                    <img class="imagem-quizz" src="${quizzSelecionado.questions[i].answers[j].image}"
+                        alt="resposta1">
+                </div>
+                <p class="p${j} texto-quizz${j}">${quizzSelecionado.questions[i].answers[j].text}</p>
+            </div>
+            `;
+        }
+    }
 }
+
+function vericarRespostas(quizzClicado) {
+    for(let i = 0; i < vetor.length; i++) {
+        if (idQuizz === vetor[i].id)  {
+            for(let j = 0; j < vetor[i].questions.length; j++) {
+                for(let k = 0; k < vetor[i].questions[j].answers.length; k++) {
+                    let imagem = document.querySelector(`.respostas${i} .imagem-quizz`);
+                    let p = document.querySelector(`.respostas${i} .texto-quizz${k}`);
+                    if (vetor[i].questions[j].answers[k].isCorrectAnswer === 'true') {
+                        respostaCerta(p, imagem);
+                    } else {
+                        respostaErrada(p, imagem);
+                    }
+                }
+            }      
+            i = vetor.length + 1; 
+        }
+    }
+}
+
+function respostaCerta(p, imagem) {
+    p.setAttribute("style", "color: #009C22");
+    // p.style.cssText = 'color: #009C22';
+    // imagem.document.classList.add("opacity");
+}
+function respostaErrada(p, imagem) {
+    // p.setAttribute("style", "color: #FF0B0B");
+    // p.style.cssText = 'color: #FF0B0B';
+    imagem.document.classList.add("opacity");
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+let vetor = [];
+
+function perguntasERespostasQuizz(quizzes) {
+
+    for (let i = 0; i < quizzes.length; i++) {
+        vetor.push(quizzes[i]);
+    }
+}
+
 
 /* -------------------------------------------- Layout Mobile (Tela 2) ----------------------------------------------- */
 
